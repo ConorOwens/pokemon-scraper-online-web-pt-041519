985c5ad65dbe3fcc71d379df0b3abc26ea6686b9
Conor Owens <conorj.owens@gmail.com>
2019-07-03 02:29:25 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", self.name, self.type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", self.name, self.type)
     @id = db.execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
